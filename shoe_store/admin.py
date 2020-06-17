8c7b6c2a6e94b927ca074462c60e1ecb70bc156e
luisfff29 <luisfff29@gmail.com>
2020-06-17 11:36:21 -0500

    create models and register in admin panel

--- shoe_store/admin.py
+++ shoe_store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from shoe_store import models
+
 
 # Register your models here.
+admin.site.register(models.Manufacturer)
+admin.site.register(models.ShoeType)
+admin.site.register(models.ShoeColor)
+admin.site.register(models.Shoe)
--- shoe_store/models.py
+++ shoe_store/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+# https://stackoverflow.com/questions/33772947/django-set-range-for-integer-model-field-as-constraint
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=200)
+    website = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class ShoeType(models.Model):
+    SHOE_TYPE = [
+        ('sneaker', 'Sneaker'),
+        ('boot', 'Boot'),
+        ('sandal', 'Sandal'),
+        ('dress', 'Dress'),
+        ('other', 'Other')
+    ]
+    style = models.CharField(max_length=200, choices=SHOE_TYPE)
+
+    def __str__(self):
+        return self.style
+
+
+class ShoeColor(models.Model):
+    SHOE_COLORS = [
+        ('red', 'Red'),
+        ('orange', 'Orange'),
+        ('yellow', 'Yellow'),
+        ('green', 'Green'),
+        ('blue', 'Blue'),
+        ('indigo', 'Indigo'),
+        ('violet', 'Violet'),
+        ('white', 'White'),
+        ('black', 'Black')
+    ]
+    color_name = models.CharField(max_length=200, choices=SHOE_COLORS)
+
+    def __str__(self):
+        return self.color_name
+
+
+class Shoe(models.Model):
+    size = models.PositiveIntegerField(
+        help_text="Max shoe size is 22",
+        validators=[MaxValueValidator(1), MinValueValidator(22)]
+    )
+    brand = models.CharField(max_length=200)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    color = models.ForeignKey(ShoeColor, on_delete=models.CASCADE)
+    material = models.CharField(max_length=200)
+    shoe_type = models.ForeignKey(ShoeType, on_delete=models.CASCADE)
+    fasten_type = models.CharField(max_length=200)
