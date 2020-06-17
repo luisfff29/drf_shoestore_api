efb68caee95d039b2007bac5f9b7fb6f5f2b94e6
luisfff29 <luisfff29@gmail.com>
2020-06-17 14:11:01 -0500

    create viewsets for shoe models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from shoe_store.models import Manufacturer, ShoeType, ShoeColor, Shoe
+from rest_framework import viewsets
+from api.serializers import (ManufacturerSerializer, ShoeTypeSerializer,
+                             ShoeColorSerializer, ShoeSerializer)
+
 
 # Create your views here.
+class ManufacturerViewSet(viewsets.ModelViewSet):
+    queryset = Manufacturer.objects.all()
+    serializer_class = ManufacturerSerializer
+
+
+class ShoeTypeViewSet(viewsets.ModelViewSet):
+    queryset = ShoeType.objects.all()
+    serializer_class = ShoeTypeSerializer
+
+
+class ShoeColorViewSet(viewsets.ModelViewSet):
+    queryset = ShoeColor.objects.all()
+    serializer_class = ShoeColorSerializer
+
+
+class ShoeViewSet(viewsets.ModelViewSet):
+    queryset = Shoe.objects.all()
+    serializer_class = ShoeSerializer
